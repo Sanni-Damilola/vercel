a4162e42bcb3ea9959491faa565e65da625a1b8f
Sanni_Damilola <114070063+Sanni-Damilola@users.noreply.github.com>
2023-01-14 07:50:52 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// app.tsx
+
+import React from "react";
+import Body from "./Components/Body";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default App;
